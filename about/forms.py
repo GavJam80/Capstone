7ff0ead063e943b733ab6cf394795a89fe39c35a
Gavin Jameson <m2pyeoeh@students.codeinstitute.net>
2025-01-23 13:58:36 +0000

    added map to index.html

--- about/forms.py
+++ about/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import CollaborateRequest, AboutPage
 from django_summernote.widgets import SummernoteWidget
 
-# Define a form class for users to request a collaboration
+# Define a form class for users to request response for more info
 class CollaborateForm(forms.ModelForm):
     """
     Form class for users to request a collaboration
